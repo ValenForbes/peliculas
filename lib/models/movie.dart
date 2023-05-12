04eb2865a6df0e2edb6643479e69d97058d9b35b
Valentín Forbes <valenforbes@gmail.com>
2023-05-12 11:47:19 -0300

    Creación de peliculas

---
+++ lib/models/movie.dart
@@ -0,0 +1,70 @@
+import 'dart:convert';
+
+class Movie {
+  Movie({
+    required this.adult,
+    this.backdropPath,
+    required this.genreIds,
+    required this.id,
+    this.originalLanguage,
+    required this.originalTitle,
+    required this.overview,
+    required this.popularity,
+    this.posterPath,
+    this.releaseDate,
+    required this.title,
+    required this.video,
+    required this.voteAverage,
+    required this.voteCount,
+  });
+
+  bool adult;
+  String? backdropPath;
+  List<int> genreIds;
+  int id;
+  String? originalLanguage;
+  String originalTitle;
+  String overview;
+  double popularity;
+  String? posterPath;
+  DateTime? releaseDate;
+  String title;
+  bool video;
+  double voteAverage;
+  int voteCount;
+
+  get fullPosterImg {
+    if (posterPath != null)
+      // ignore: curly_braces_in_flow_control_structures, unnecessary_brace_in_string_interps
+      return 'https://image.tmdb.org/t/p/w500${posterPath}';
+
+    return 'https://i.stack.imgur.com/GNhx0.png';
+  }
+
+  get fullBackdropPath {
+    if (backdropPath != null)
+      // ignore: curly_braces_in_flow_control_structures, unnecessary_brace_in_string_interps
+      return 'https://image.tmdb.org/t/p/w500${backdropPath}';
+
+    return 'https://i.stack.imgur.com/GNhx0.png';
+  }
+
+  factory Movie.fromRawJson(String str) => Movie.fromJson(json.decode(str));
+
+  factory Movie.fromJson(Map<String, dynamic> json) => Movie(
+        adult: json["adult"],
+        backdropPath: json["backdrop_path"],
+        genreIds: List<int>.from(json["genre_ids"].map((x) => x)),
+        id: json["id"],
+        originalLanguage: json['original_languaje'],
+        originalTitle: json["original_title"],
+        overview: json["overview"],
+        popularity: json["popularity"]?.toDouble(),
+        posterPath: json["poster_path"],
+        releaseDate: DateTime.parse(json["release_date"]),
+        title: json["title"],
+        video: json["video"],
+        voteAverage: json["vote_average"]?.toDouble(),
+        voteCount: json["vote_count"],
+      );
+}
---
+++ lib/search/search_delegate.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/providers/movies_provider.dart';
+import 'package:provider/provider.dart';
+
+import '../models/models.dart';
+
+class MovieSearchDelegate extends SearchDelegate {
+  @override
+  String get searchFieldLabel => 'Buscar Película';
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+          onPressed: () {
+            query = '';
+          },
+          icon: const Icon(Icons.clear))
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+        onPressed: () {
+          close(context, null);
+        },
+        icon: const Icon(Icons.arrow_back));
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return const Text('buildResults');
+  }
+
+  Widget _emptySizedBox() {
+    return const SizedBox(
+        child: Center(
+      child: Icon(
+        Icons.movie_creation_outlined,
+        color: Colors.black38,
+        size: 100,
+      ),
+    ));
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    if (query.isEmpty) {
+      return _emptySizedBox();
+    }
+
+    final moviesProvider = Provider.of<MoviesProvider>(context, listen: false);
+    return FutureBuilder(
+      future: moviesProvider.searchMovies(query),
+      builder: (_, AsyncSnapshot<List<Movie>> snapshot) {
+        if (!snapshot.hasData) return _emptySizedBox();
+        final movies = snapshot.data!;
+        return ListView.builder(
+            itemCount: movies.length,
+            itemBuilder: (_, int index) => _MovieItem(movies[index]));
+      },
+    );
+  }
+}
+
+class _MovieItem extends StatelessWidget {
+  final Movie movie;
+
+  const _MovieItem(this.movie);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: FadeInImage(
+        placeholder: const AssetImage('assets/no-image.jpg'),
+        image: NetworkImage(movie.fullPosterImg),
+        width: 50,
+        fit: BoxFit.contain,
+      ),
+      title: Text(movie.title),
+      subtitle: Text(movie.originalTitle),
+      onTap: () {
+        Navigator.pushNamed(context, 'details', arguments: movie);
+      },
+    );
+  }
+}
